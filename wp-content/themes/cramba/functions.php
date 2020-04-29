ccb81c1e9c3de76437cd5923600b0e4ddf39848c
Carson Reinke <carson@reinke.co>
2020-04-28 20:45:12 -0400

    WordPress does not support grandchild themes

--- wp-content/themes/cramba/functions.php
+++ wp-content/themes/cramba/functions.php
@@ -2,7 +2,7 @@
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
  
-    $parent_style = 'lyrical'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $parent_style = 'primer';
  
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'cramba',
