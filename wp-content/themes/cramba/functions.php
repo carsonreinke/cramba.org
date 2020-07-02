80baf703b19d619f1a507f7e0f49cfb0e440a352
Carson Reinke <carson@reinke.co>
2020-07-02 13:53:36 -0400

    Revert "Correct WordPress emoji prefetch removal"
    This reverts commit 069ad4078f9751d873f16bdbdacc654fa854b7c2.

--- wp-content/themes/cramba/functions.php
+++ wp-content/themes/cramba/functions.php
@@ -711,7 +711,7 @@ function disable_emojis_tinymce( $plugins ) {
 function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
 	if ( 'dns-prefetch' == $relation_type ) {
 		/** This filter is documented in wp-includes/formatting.php */
-		$emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/12.0.0-1/svg/' );
+		$emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
 
 		$urls = array_diff( $urls, array( $emoji_svg_url ) );
 	}
